c05b629f19974b932748057ddd58510dba2289db
Billy Le <hi@billyle.dev>
2023-08-03 01:30:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Beastify
+
+My first Firefox browser extenstion by following their [tutorial](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension).
---
+++ content_scripts/beastify.js
@@ -0,0 +1,28 @@
+(() => {
+  if (window.hasRun) return;
+  window.hasRun = true;
+
+  function insertBeast(beastURL) {
+    removeExistingBeast();
+    const beastImage = document.createElement("img");
+    beastImage.setAttribute("src", beastURL);
+    beastImage.style.height = "100vh";
+    beastImage.className = "beastify-image";
+    document.body.appendChild(beastImage);
+  }
+
+  function removeExistingBeast() {
+    const existingBeasts = document.querySelectorAll(".beastify-image");
+    for (const beast of existingBeasts) {
+      beast.remove();
+    }
+  }
+
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "beastify") {
+      insertBeast(message.beastURL);
+    } else if (message.command === "reset") {
+      removeExistingBeasts();
+    }
+  });
+})();
